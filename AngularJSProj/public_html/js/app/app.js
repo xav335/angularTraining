c419aa2e27ef382b4cad9b9ea0413c13f4632023
Xavier Gonzalez <xavier@gonzalez.pm>
2015-05-20 12:03:01 +0200

    definition var nom à faire 1ere posibilité

--- AngularJSProj/public_html/js/app/app.js
+++ AngularJSProj/public_html/js/app/app.js
@@ -1,3 +1,12 @@
 (function (){
     var app = angular.module("MonAppli", []);
+    
+    /*
+     * Controleurs 
+     */
+    app.controller('SalutationController', function(){
+        //alert('OK');
+        this.nom = 'toto';
+    });
+    
 })();
