8f0104e74e8a0db7cc57111bdbc315a5c66b688c
Xavier Gonzalez <xavier@gonzalez.pm>
2015-05-20 14:01:12 +0200

    tableau avec controleur

---
+++ AngularJSProj/public_html/js/app/app_hello.js
@@ -0,0 +1,79 @@
+(function (){
+    var app = angular.module("ColectivTdos", []);
+    
+    /*
+     * Controleurs 
+     */
+    var todos = [
+        {
+            titre:'Préparer la salle de formation',
+            date:'2015-05-17',
+            importance:1,
+            realisee:false,
+            commentaires:[
+                {
+                    auteur:'xavier@gonzalez.pm',
+                    date:'2015-05-17',
+                    commentaire:'Tache super simple'
+                },
+                {
+                    auteur:'xav335@hotmail.com',
+                    date:'2015-05-18',
+                    commentaire:'c\'est pas tout à fait vrai ça !'
+                },
+                {
+                    auteur:'xavier@gonzalez.pm',
+                    date:'2015-05-17',
+                    commentaire:'Mais si mais si'
+                }
+            ]
+            
+        },
+         {
+            titre:'Trouver une formation pour le stage',
+            date:'2015-05-17',
+            importance:1,
+            realisee:false,
+            commentaires:[
+                {
+                    auteur:'xavier@gonzalez.pm',
+                    date:'2015-05-17',
+                    commentaire:'Tache super à ne pas confier à des choukis'
+                },
+                {
+                    auteur:'xav335@hotmail.com',
+                    date:'2015-05-18',
+                    commentaire:'je sui spas un chouki !'
+                },
+                {
+                    auteur:'xavier@gonzalez.pm',
+                    date:'2015-05-17',
+                    commentaire:'Si ça je te le confirme !'
+                }
+            ]
+            
+        },
+         {
+            titre:'Trouver les transports necessaire',
+            date:'2015-05-17',
+            importance:1,
+            realisee:false,
+            commentaires:[
+                {
+                    auteur:'xavier@gonzalez.pm',
+                    date:'2015-05-17',
+                    commentaire:'Tache très compliquée'
+                }
+            ]
+            
+        }
+        
+    ];
+    
+    app.controller('TodosController', function($scope){
+        //alert('OK');
+        $scope.todos = todos;
+    });
+    
+})();
+
