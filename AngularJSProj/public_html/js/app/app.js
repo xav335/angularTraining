232a9e0216a289508184fca9ecbfc80f13d251db
Xavier Gonzalez <xavier@gonzalez.pm>
2015-05-21 09:12:49 +0200

    recup projet Mathilde + panneau info commentaire

--- AngularJSProj/public_html/js/app/app.js
+++ AngularJSProj/public_html/js/app/app.js
@@ -1,12 +1,123 @@
-(function (){
-    var app = angular.module("MonAppli", []);
+
+(function(){
+    var app = angular.module('CollectiveTodos', []);
     
-    /*
-     * Controleurs 
-     */
-    app.controller('SalutationController', function($scope){
-        //alert('OK');
-        $scope.nom = 'anonyme';
-    });
+    var todos = [
+        {
+            titre:'Préparer la salle de formation',
+            date:'2015-05-16',
+            description:'Installer les poster, régler le rétroprojecteur et vider la poubelle.',
+            importance:1,
+            realisee:false,
+            commentaires:[
+                {
+                    auteur:'ericguyader@yahoo.fr',
+                    date:'2015-05-16',
+                    message:'Tâche de la plus haute importance et à ne confier qu\'à des personnes qualifiées !'
+                },
+                {
+                    auteur:'pauldurand@yahoo.fr',
+                    date:'2015-05-17',
+                    message:'+1!'
+                },
+                {
+                    auteur:'marcopaulo@discover.net',
+                    date:'2015-05-18',
+                    message:'Je peux m\'en charger...'
+                }
+            ]
+        },
+        {
+            titre:'Trouver un formateur pour le stage "Regular.JS"',
+            date:'2015-05-19',
+            description:'Je pensais avoir trouvé quelqu\'un mais en fait... non...',
+            importance:2,
+            realisee:true,
+            commentaires:[
+                {
+                    auteur:'pierrekiroule@nomousse.com',
+                    date:'2015-05-19',
+                    message:'Ne comptez pas sur moi !'
+                },
+                {
+                    auteur:'pauldurand@yahoo.fr',
+                    date:'2015-05-17',
+                    message:'-1!'
+                }
+            ]
+        },
+        {
+            titre:'Organiser l\'anniversaire de Martine',
+            date:'2015-05-20',
+            description:'Préparer la petite fête d\'anniversaire traditionnelle de Martine.',
+            importance:3,
+            realisee:false,
+            commentaires:[
+                {
+                    auteur:'ericguyader@yahoo.fr',
+                    date:'2015-05-20',
+                    message:'J\'ai trouvé pour acheter de quoi boire !'
+                },
+                {
+                    auteur:'pauldurand@yahoo.fr',
+                    date:'2015-05-20',
+                    message:'+10 :-)'
+                }
+            ]
+        }
+    ];
     
+    app.controller('tachesController', ['$scope',function($scope){
+        $scope.nostaches = todos;
+        
+        $scope.selectionTache = function(tache){
+            $scope.detailsTache = tache;
+            
+        };
+        $scope.infoActif = true;
+        $scope.selectionPanneau = function(infoActif){
+            if (infoActif){
+                $scope.infoActif = false;
+                $scope.commActif = true;
+            } else {
+                $scope.infoActif = true;
+                $scope.commActif = false;
+            }
+           
+            
+        };
+        
+    }]);
+
+    app.filter('importance', ['$sce', function($sce){
+            $sce.trustAsHtml();
+        return function(input,icones){
+            icones = icones || false;
+            if(!icones){
+                switch(input){
+                    case 1:
+                        return 'Haute';
+                    case 2:
+                        return 'Moyenne';
+                    case 3:
+                        return 'Basse';
+                    default:
+                        return input;
+                }
+            }else{
+                switch(input){
+                    case 1:
+                        return 'glyphicon glyphicon-thumbs-up text-danger';
+                    case 2:
+                        return 'glyphicon glyphicon-hand-right';
+                    case 3:
+                        return 'glyphicon glyphicon-thumbs-down text-success';
+                    default:
+                        return input;
+                }
+            }
+        };
+    }]);
 })();
+
+
--- AngularJSProj/public_html/js/app/app_hello.js
+++ AngularJSProj/public_html/js/app/app_hello.js
@@ -1,118 +1,19 @@
-(function (){
-    var app = angular.module("ColectivTdos", []);
-    
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+(function(){
+    var app = angular.module(
+            'MyApp',[]
+            );
     /*
-     * Controleurs 
+     * Contrôleurs
      */
-    var todos = [
-        {
-            titre:'Préparer la salle de formation',
-            description:'description de Préparer la salle de formation',
-            date:'2015-05-12',
-            importance:1,
-            realisee:false,
-            commentaires:[
-                {
-                    auteur:'xavier@gonzalez.pm',
-                    date:'2015-05-17',
-                    commentaire:'Tache super simple'
-                },
-                {
-                    auteur:'xav335@hotmail.com',
-                    date:'2015-05-18',
-                    commentaire:'c\'est pas tout à fait vrai ça !'
-                },
-                {
-                    auteur:'xavier@gonzalez.pm',
-                    date:'2015-05-17',
-                    commentaire:'Mais si mais si'
-                }
-            ]
-            
-        },
-         {
-            titre:'Trouver une formation pour le stage',
-            description:'description de Trouver une formation pour le stage',
-            date:'2015-05-17',
-            importance:3,
-            realisee:false,
-            commentaires:[
-                {
-                    auteur:'xavier@gonzalez.pm',
-                    date:'2015-05-02',
-                    commentaire:'Tache super à ne pas confier à des choukis'
-                },
-                {
-                    auteur:'xav335@hotmail.com',
-                    date:'2015-05-18',
-                    commentaire:'je sui spas un chouki !'
-                },
-                {
-                    auteur:'xavier@gonzalez.pm',
-                    date:'2015-05-17',
-                    commentaire:'Si ça je te le confirme !'
-                }
-            ]
-            
-        },
-         {
-            titre:'Trouver les transports necessaire',
-            description:'description de s transports necessaire',
-            date:'2015-05-17',
-            importance:2,
-            realisee:true,
-            commentaires:[
-                {
-                    auteur:'xavier@gonzalez.pm',
-                    date:'2015-05-17',
-                    commentaire:'Tache très compliquée'
-                }
-            ]
-            
-        }
-        
-    ];
-    
-    app.controller('TodosController', ['$scope', function($scope){
-        //alert('OK');
-        $scope.todos = todos;
-        
-        $scope.selectionTache = function (value){
-            $scope.detailTache = value;
-            if (value.date > '2015-05-12'){
-           console.log(' date: '+value.date);
-           //if (!value.realisee){
-                  $scope.panelInfo = true;
-                  $scope.panelDanger = false;
-            }else{
-                  $scope.panelInfo = false;
-                  $scope.panelDanger = true;
-            }
-        };
-        
-        $scope.effaceTache = function (){
-            $scope.detailTache = null;
-        };
-        
-        
-    }]);
-
-    app.filter('importance', function(){
-        return function(input){
-            switch (input){
-                case 1:
-                    return 'Basse';
-                case 2:
-                    return 'Moyenne';
-                case 3:
-                    return 'Haute';
-                default:
-                    return input;
-            }
-        }
+    //angular.module('MyApp')
+    // OU, plus simplement : 
+    app.controller('SalutationController',function($scope){
+       $scope.nom = 'Anonyme'; 
     });
-
-
-    
-})();
-
+}) ();
